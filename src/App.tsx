58a8701f44eedc5c392017f0bd6140a6d066cb6e
lukas <lukas.arnroth@gmail.com>
2020-08-21 09:57:52 +0200

    adding web to git

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import Header from './components/Header';
+import Body from './components/Body';
+import Footer from './components/Footer';
+
+const MainDiv = styled.div`
+  width: 100%;
+  margin: 0 auto;
+`;
+
+const useViewport = () => {
+  const [width, setWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleWindowResize = () => setWidth(window.innerWidth);
+    window.addEventListener('resize', handleWindowResize);
+    return () => window.removeEventListener('resize', handleWindowResize);
+  }, []);
+  return { width };
+};
 
 function App() {
+  const { width } = useViewport();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MainDiv>
+      <Header width={width} />
+      <Body width={width} />
+      <Footer />
+    </MainDiv>
   );
 }
 
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,7 @@
 /// <reference types="react-scripts" />
+
+//Removes issues with importing png files
+declare module '*.png' {
+  const value: any;
+  export = value;
+}
